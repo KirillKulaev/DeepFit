f52fbfe21c0b240a5b7b4fed75e96cac8298cff0
Kirill Kulaev <139176905+KirillKulaev@users.noreply.github.com>
2025-09-16 14:00:36 +0300

    new setup

--- setup.py
+++ setup.py
@@ -1,25 +1,28 @@
 from setuptools import setup, find_packages
+import os
 
 # Read requirements from requirements.txt
 with open("requirements.txt") as f:
     REQUIRED_PACKAGES = f.read().splitlines()
 
-PACKAGE_DATA = {
-    "deepfit_package": [
-        "model/*.npz",
-        "model/*.py",
-        "model/weights/*",
-    ]
-}
+# Get package directory
+package_dir = "deepfit_package"
 
 setup(
-    name="deepfit_package", 
+    name="deepfit_package",
     version="0.1.0",
     description="DeepFit: physically and chemically informed XAS-Structure fitting made simple",
     url="https://github.com/KirillKulaev/DeepFit",
     author="Kirill Kulaev, Bogdan Procenko",
-    packages=find_packages(include=["deepfit_package", "deepfit_package.*"]),
-    package_data=PACKAGE_DATA,
+    author_email="<your-email@example.com>",
+    packages=find_packages(),
+    package_dir={"deepfit_package": "deepfit_package"},
+    package_data={
+        "deepfit_package": [
+            "model/*.npz",
+            "model/weights/*",
+        ]
+    },
     include_package_data=True,
     install_requires=REQUIRED_PACKAGES,
     python_requires=">=3.7",
