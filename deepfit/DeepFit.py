273753885e6caddf63c8ccffd96caf6a5e899ed4
Kirill Kulaev <139176905+KirillKulaev@users.noreply.github.com>
2025-09-16 13:06:51 +0300

    bug fix

--- deepfit/DeepFit.py
+++ deepfit/DeepFit.py
@@ -5,7 +5,7 @@ import numpy as np
 from tqdm import tqdm
 from torch_geometric.data import Data
 
-descriptors = np.load(f'descriptors.npz')
+descriptors = np.load(f'model/descriptors.npz')
 
 periodic_table = {'h': 1, 'he': 2, 'li': 3, 'be': 4, 'b': 5, 'c': 6, 'n': 7, 'o': 8, 'f': 9, 'ne': 10, 
                   'na': 11, 'mg': 12, 'al': 13, 'si': 14, 'p': 15, 's': 16, 'cl': 17, 'ar': 18, 
